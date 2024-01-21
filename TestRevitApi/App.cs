524ae2fce6ba64f327d73d5f99570b130a958498
Nguyen van Phong <ngvanphong2012@gmail.com>
2024-01-21 10:57:03 +0700

    Add project files.

---
+++ TestRevitApi/App.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRevitApi
+{
+    internal class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ TestRevitApi/ElementName.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRevitApi
+{
+    internal class ElementName
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value;}
+        }
+
+
+        private Element element;
+        public Element Element
+        { 
+            get { return element; }
+            set{element = value;}
+        }
+    }
+}
